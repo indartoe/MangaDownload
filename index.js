bb1870bf36298db92b3499dd63b431b4ea0bb4c8
ShinoaShiro <38904236+ShinoaShiro@users.noreply.github.com>
2021-04-12 19:05:19 +0700

    Add files via upload
    Using puppetter

--- index.js
+++ index.js
@@ -1,51 +1,84 @@
 var Crawler = require("crawler");
 var fs = require('fs');
-var arrayManga = new Array();
- 
-var c = new Crawler({
-    // encoding:null,
-    // jQuery:false,// set false to suppress warning message.
-    callback:function(err, res, done){
-        if(err){
-            console.error(err.stack);
-        }else{
-            var $ = res.$;
-            var tempImg = res.$("img");
-            for (let x = 0; x < tempImg.length ; x++) {
-                if (tempImg[x]['attribs']['class'] != null) {
-                    if (!tempImg[x]['attribs']['class'].includes("logo")) {
-                        arrayManga.push(tempImg[x]['attribs']['src']);
-                    }
-                }
-            }
-            if (arrayManga.length > 0) {
-                for (let index = 0; index < arrayManga.length; index++) {
-                    c.queue({
-                        uri:arrayManga[index],
-                        filename:"Manga_Trial" + index + ".png",
-                        encoding:null,
-                        jQuery:false,// set false to suppress warning message.
-                        callback: function (error, res, done) {
-                            if(error){
-                                console.log(error);
-                            }else{
-                                fs.createWriteStream(res.options.filename).write(res.body);
-                            }
-                            done();
-                        }
-                    });
-                }
-            }
-            // fs.createWriteStream(res.options.filename).write(res.body);
+var fsRead = require('fs');
+const puppeteer = require("puppeteer");
+const http = require('http'); // or 'https' for https:// URLs
+var i = 1;
+
+loadMangaImage();
+
+function loadMangaImage() {
+    (async () => {
+        try {
+          // Initialize Puppeteer
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+      
+          // Specify comic issue page url
+          await page.goto(
+            "http://mangareader.cc/chapter/komi-san-wa-komyushou-desu-chapter-298#1"
+          );
+          console.log("page has been loaded!");
+      
+          // While page is waiting for 1s, click on the 'Full Chapter' button and do the rest
+        //   await page.waitFor(5000);
+          await page.select("select.loadImgType.pull-left", "1");
+          await page.waitFor(5000);
+          // await page.click("button.button4");
+          console.log("'Full Chapter' button has been clicked!");
+      
+          // Evaluate/Compute the main task:
+          // Here, we convert the Nodelist of images returned from the DOM into an array, then map each item and get the src attribute value, and store it in 'src' variable, which is therefore returned to be the value of 'issueSrcs' variable.
+          const issueSrcs = await page.evaluate(() => {
+            const srcs = Array.from(
+              document.querySelectorAll(".lazy")
+            ).map((image) => image.getAttribute("src"));
+            return srcs;
+          });
+          // issueSrcs.split();
+          // for (let index = 0; index < array.length; index++) {
+          //     console.log(issueSrcs[index]);
+          // }
+          console.log("Page has been evaluated!");
+      
+          // Persist data into data.json file
+        //   fs.writeFileSync("./data.json", JSON.stringify(issueSrcs));
+        //   console.log("File is created!");
+      
+          // End Puppeteer
+          await browser.close();
+          for (let index = 0; index < issueSrcs.length; index++) {
+              crawlImage(issueSrcs[index], index);
+          }
+        } catch (error) {
+          console.log(error);
         }
-        done();
-    }
-});
+      })();
+}
 
-c.queue({
-    uri:"https://mangahub.io/chapter/uzaki-chan-wa-asobitai/chapter-65"
-});
-
+function crawlImage(urlLink, varSequence) {
+    const file = fs.createWriteStream("Manga_Trial" + varSequence + ".png");
+    const request = http.get(urlLink, function(response) {
+        response.pipe(file);
+    });
+    // var c = new Crawler({
+    //     filename:"Manga_Trial" + varSequence + ".png",
+    //     encoding:null,
+    //     jQuery:false,// set false to suppress warning message.
+    //     callback:function(err, res, done){
+    //         if(err){
+    //             console.error(err.stack);
+    //         }else{
+    //             fs.createWriteStream(res.options.filename).write(res.body);
+    //         }
+    //         done();
+    //     }
+    // });
+    
+    // c.queue({
+    //     uri:urlLink
+    // });
+}
 
 // const c = new Crawler({
 //     maxConnections : 10,
@@ -107,4 +140,4 @@ c.queue({
 // Queue some HTML code directly without grabbing (mostly for tests)
 // c.queue([{
 //     html: '<p>This is a <strong>test</strong></p>'
-// }]);
+// }])-;
